4c8168d00573ef9699ba5ab58c4129307df15fc3
Alpatron <1508117+alpatron@users.noreply.github.com>
2021-07-05 17:59:23 +0200

    Attain parity with Heroku version

--- init.py
+++ init.py
@@ -5,17 +5,15 @@ from discord.ext import commands
 from main import RegBot
 
 def init():
-    #Change this line to suit your environment; it's set-up now for deployment to Heroku.
-    credentials = {"dsn":os.environ['DATABASE_URL']}
-    
     loop = asyncio.get_event_loop()
-
+    
     try:
         db = loop.run_until_complete(
-           asyncpg.create_pool(**credentials)
+           asyncpg.create_pool(dsn=os.environ['DATABASE_URL'],ssl='require')
         )
-    except Exception:
+    except Exception as e:
         print('Database connection error!')
+        print(e)
         return
 
     bot = RegBot(db)
